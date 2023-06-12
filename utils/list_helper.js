ca398d6677fc4e93aa3d00c3069a36af2e86525e
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-12 20:06:24 +0430

    4.5 helper funcs & unit tests step3 favoriteBlog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,8 @@
 const dummy = (blogs) => 1;
 const totalLikes = (blogs) => blogs.reduce((sum, item) => sum + item.likes, 0);
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  const likesArray = blogs.map((b) => b.likes);
+  const maxIndex = likesArray.indexOf(Math.max(...likesArray));
+  return blogs[0] ? blogs[maxIndex] : [];
+};
+module.exports = { dummy, totalLikes, favoriteBlog };
