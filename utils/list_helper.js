edf00f03cd99b5df792f4141b0062033e018d5b2
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-12 22:10:07 +0430

    4.6 helper funcs & unit tests step4 mostBlogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+const { info } = require('./logger');
 const dummy = (blogs) => 1;
 const totalLikes = (blogs) => blogs.reduce((sum, item) => sum + item.likes, 0);
 const favoriteBlog = (blogs) => {
@@ -5,4 +7,11 @@ const favoriteBlog = (blogs) => {
   const maxIndex = likesArray.indexOf(Math.max(...likesArray));
   return blogs[0] ? blogs[maxIndex] : [];
 };
-module.exports = { dummy, totalLikes, favoriteBlog };
+const mostBlogs = (blogs) => {
+  const grouped = _.groupBy(blogs, 'author');
+  const blogAuthors = Object.keys(grouped);
+  const blogsCount = Object.values(grouped).map((g) => g.length);
+  const ind = blogsCount.indexOf(Math.max(...blogsCount));
+  return blogs[0] ? { author: blogAuthors[ind], blogs: blogsCount[ind] } : [];
+};
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
