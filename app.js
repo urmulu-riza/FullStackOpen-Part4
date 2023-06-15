545e348eb684f8edd0a4572cb9135ad18486b8f5
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-15 11:54:18 +0430

    4.18 Bloglist expansion, Step6: token-based authentication

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
+const loginRouter = require('./controller/login');
 const blogsRouter = require('./controller/blogs');
 const usersRouter = require('./controller/users');
 const mongoose = require('mongoose');
@@ -25,6 +26,7 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
